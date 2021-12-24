0c052e9cea387f2e456f4050ea6b162a9bd90e21
HRITHIK MALLICK <66324529+hrithikmallick@users.noreply.github.com>
2021-12-24 21:57:20 +0530

    done

--- demo.py
+++ demo.py
@@ -1,8 +1,13 @@
-paswo
+password = "hello1234@"
+spl = 0
+temp = 0
 for i in list(password):
-    ten = 0
+    # print(i)
     if i in "[@_!#$%^&*()<>?/|}{~:]":
-
-        wrt = wrt+1
-    else:
-        ten = 1
+        spl = spl+1
+        break
+if spl == 0:
+    temp = 1
+    print("does not have a special char")
+if temp == 0:
+    print("pass does have a special char")
--- emailVerif.py
+++ emailVerif.py
@@ -5,21 +5,21 @@ rePassword = input("Re enter the password: ")
 temp = 0
 wrt = 0
 
-
+# CHECK FIRST CHAR IS UPPERCASE
 if password[0].isupper():
     wrt = wrt+1
 else:
     temp = 1
     print("first character can not be in small case")
 
-
+# CHECK USER NAME IN IT
 if userName in password:
     temp = 1
     print("your password can not contain user name")
 else:
     wrt = wrt+1
 
-
+# checking the length
 if len(password) >= 8 or len(password) >= 16:
     print(len(password))
     wrt = wrt+1
@@ -28,22 +28,23 @@ else:
     print(len(password))
     print("Your password has be range of 8 to 16")
 
-
+# for checking both password are same
 if password == rePassword:
     wrt = wrt+1
 else:
     print("please give same password in both fild")
     temp = 1
 
-
+# for checking special char
+spl = 0
 for i in list(password):
-    spl = 0
+    # print(i)
     if i in "[@_!#$%^&*()<>?/|}{~:]":
         spl = spl+1
         break
-    if spl > 1:
-        temp = 1
-
+if spl == 0:
+    temp = 1
+    print("does not have a special char")
 
 # print(temp)
 if temp == 0:
