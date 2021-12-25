d213d7cc3172b11619ec45b60bdd36bb82950b02
HRITHIK MALLICK <66324529+hrithikmallick@users.noreply.github.com>
2021-12-25 22:35:49 +0530

    email verifier

--- emailVerif.py
+++ emailVerif.py
@@ -1,15 +1,16 @@
 def emailVerifier(email):
     lenEm = len(email)
     print("length of your email is :- ", lenEm)
+    del lenEm
     temp = 0
-
+# CHECKING @ IN EMAIL
     if (email.find('@') == -1):
         print("@ missing")
         temp = temp+1
 
     apt = email.find("@")
     dot = email.find(".")
-
+# CHECKING DOMAIN IN EMAIL
     if (email.find('@') != -1):
         if email.find(".", apt) < email.find("@"):
             print("invalid check domain again")
@@ -17,11 +18,15 @@ def emailVerifier(email):
     else:
         if email.find(".") == -1:
             print(". is also missing")
+# CHECKING PROVIDER
     if (dot-apt) == 1:
         print("provider is missing")
         temp = temp+1
 
     del apt
+    del dot
+
+# ATLAST RESULT
     if temp == 0:
         print("It is a vaild email")
 
